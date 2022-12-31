f3c1ab85e1c0cd8db88857242629a3e74e0667f2
Amir Ahmed <amir.ahmed79@outlook.com>
2022-12-31 18:56:49 -0500

    added styled components

---
+++ frontend/components/Products.js
@@ -0,0 +1,14 @@
+export default function Product({product}){
+    return(
+        <div>
+           <div>
+          <img scr=" " alt=""/>  
+        </div> 
+        <div>
+    <h2>title</h2>
+    <h3>price</h3>
+
+        </div>
+        </div>
+    )
+}
